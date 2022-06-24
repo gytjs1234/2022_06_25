74708f5df699b67c11ead60035ffd835cde5db61
gytjs1234 <dkvkxmdnal@naver.com>
2022-06-25 00:20:41 +0900

    이게진짜당

--- SpMVC_08_Memo/src/main/java/com/callor/memo/persistance/MemoDao.java
+++
@@ -1,7 +0,0 @@
-package com.callor.memo.persistance;
-
-import com.callor.memo.model.MemoVO;
-
-public interface MemoDao extends GenericDao<MemoVO, String>{
-	public void create_memo_table();
-}
--- SpMVC_08_Memo/src/main/java/com/callor/memo/service/MemoService.java
+++
@@ -1,9 +0,0 @@
-package com.callor.memo.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-import com.callor.memo.persistance.MemoDao;
-
-public interface MemoService extends MemoDao {
-	public String fileUp(MultipartFile file) throws Exception;
-}
